c3f1abe1a8323bd8994cdfdd67a5cebc51acf27f
Aliie25 <182410102033@students.unej.ac.id>
2020-10-12 19:41:40 +0700

    Kelompok *

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MyApp());
 }
 
-//initt
+//init
 //test commit
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Kelompok 8 SDGS TI_A'),
+      home: MyHomePage(title: 'Kelompok 8 SDGS TI_A '),
     );
   }
 }
