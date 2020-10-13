90fad5fe9581e12b328d90175050a7fa10d8bd48
Aliie25 <182410102033@students.unej.ac.id>
2020-10-13 14:14:56 +0700

    Kelompok 8 SDGS

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Kelompok 8 SDGS'),
     );
   }
 }
